c5c4e9fa20905b663cc96e601319bcc6a87cbd83
MartinKing01 <martinjakab01@gmail.com>
2023-04-21 09:39:34 +0200

    created firing mode all

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,7 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
+        return true;
         //TODO implement feature
 
         break;
